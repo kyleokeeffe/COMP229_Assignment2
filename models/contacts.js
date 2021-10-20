0b4e94b2b76dbcdd8dbfe2803caa4a09bb67fe22
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2021-10-20 19:01:28 -0400

    Created contact model schema

---
+++ models/contacts.js
@@ -0,0 +1,45 @@
+let mongoose = require('mongoose');
+ let passportLocalMongoose = require('passport-local-mongoose');
+
+ let User = mongoose.Schema(
+     {
+         username: 
+         {
+             type: String, 
+             default: '', 
+             trim: true,
+             required: 'username is required'
+         },
+         //if had password would look like 
+         password:{
+             type: String,
+             default:'',
+             required: 'Password is requried'
+         },
+         email:{
+             type: String,
+             default:'',
+             required: 'Email is requried'
+         },
+         displayName:{
+             type: String,
+             default:'',
+             required: 'Display Name is requried'
+         },
+         created:{
+             type: Date,
+             default: Date.now         },
+         update:{
+             type: Date,
+             default: Date.now,
+             }
+     },
+     {
+         collection:"users"
+     }
+ );
+
+ //Configure options for model 
+ let options = ({ missingPasswordError: 'Wrong/Missing password'});
+ User.plugin(passportLocalMongoose, options);
+ module.exports.user = mongoose.model('User', User);
